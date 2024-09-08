8247464124e195cb2da16eb0c4295d2442fa3724
fkasnar <fk.fkasnar@gmail.com>
2024-09-08 16:03:01 +0200

    sidebar positioning fix

--- napredni_php/views/partials/sidebar.php
+++ napredni_php/views/partials/sidebar.php
@@ -1,10 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $pageTitle ?? 'Videoteka Admin' ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/public/assets/styles.css">
+  </head>
+  <body>
+
+
+
 <aside class="d-flex flex-column p-3 text-bg-dark vh-100-min" style="width: 280px;">
+    
+
+    <div class="sidebar_position">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-auto me-md-auto text-white text-decoration-none">
         <span class="fs-4">Videoteka Admin</span>
     </a>
-
     <hr>
-
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
             <a href="/" class="nav-link text-white link-primary <?= setActiveClass('/') ?>" <?= setAriaCurent('/') ?>>
@@ -47,20 +62,8 @@
             </a>
         </li>
     </ul>
-
     <hr>
-
-    <div class="dropdown">
-        <a href="#" class="d-flex p-1 align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-            <strong>Korisnik</strong>
-        </a>
-        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Sign out</a></li>
-        </ul>
     </div>
+
+
 </aside>
