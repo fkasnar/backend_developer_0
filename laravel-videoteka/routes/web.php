25c7e0467e0093923ca567a1078556920ccabad1
fkasnar <fk.fkasnar@gmail.com>
2024-09-15 18:32:21 +0200

    add assets

--- laravel-videoteka/routes/web.php
+++ laravel-videoteka/routes/web.php
@@ -1,23 +1,32 @@
 <?php
 
 use App\Models\Price;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.home');
 });
+
 
 Route::get('/prices', function () {
 
     $prices = Price::all();
 
-    return view('prices.index', [
+    return view('admin.prices.index', [
         'prices' => $prices
     ]);
 });
 
-Route::post('/prices', function () {
-    return view('welcome');
+Route::get('/prices/create', function() {
+    return view('admin.prices.create');
 });
+
+Route::post('/prices', function (Request $request) {
+    Price::validate($request->all());
+});
+
+
 
 // ORM - Object Relational Mapping
